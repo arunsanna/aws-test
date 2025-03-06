e530da5313c59bb69b3cb4437638df1f3b282400
Mr.A <arun.sanna@outlook.com>
2025-03-05 23:34:05 -0500

    Add cis_cloudtrail.sh script for AWS CloudTrail CIS compliance checks

--- scripts/cis_cloudtrail.sh
+++ scripts/cis_cloudtrail.sh
@@ -12,30 +12,30 @@ cat_total=
 percent=0
 
 #generate the cloudtrail list and save it into a  json.
-aws cloudtrail describe-trails > traillist.json &&
+aws cloudtrail describe-trails > ../data/traillist.json &&
 
 #Ensure cloudtrail with IsMultiRegionTrail: true
-if grep -q '"IsMultiRegionTrail": true' ./traillist.json; then
+if grep -q '"IsMultiRegionTrail": true' ../data/traillist.json; then
 	cis_2_1=1
 fi
 
 #Esure Logfile validation enabled for the cloudtrail
-if grep -q '"LogFileValidationEnabled": true' ./traillist.json; then
+if grep -q '"LogFileValidationEnabled": true' ../data/traillist.json; then
 	cis_2_2=1
 fi
 
 #Ensure Cloudwatchlogsgropusarn is not empty it the value is empty this will not showup in the policy of trail
-if grep -q '"CloudWatchLogsLogGroupArn"' ./traillist.json; then
+if grep -q '"CloudWatchLogsLogGroupArn"' ../data/traillist.json; then
 	cis_2_4=1
 fi
 
 #Ensure that each trail has KMS property defined
-if grep -q '"KmsKeyID"' ./trailist.json; then
+if grep -q '"KmsKeyID"' ../data/traillist.json; then
 	cis_2_7=1
 fi
 
 #ensure bucket attached to cloud trail is not plublicly accesible
-trail_bucket=$(cat traillist.json | jq '.trailList[0] .S3BucketName' | tr -d '"')
+trail_bucket=$(cat ../data/traillist.json | jq '.trailList[0] .S3BucketName' | tr -d '"')
 buc_usr=$(aws s3api get-bucket-acl --bucket $trail_bucket --query 'Grants[?Grantee.URI==`http://acs.amazonaws.com/groups/global/AllUsers`]'| tr -d "[,]")
 buc_auth_usr=$(aws s3api get-bucket-acl --bucket $trail_bucket --query 'Grants[?Grantee.URI==`http://acs.amazonaws.com/groups/global/Authenticated Users`]' | tr -d "[,]")
 
@@ -103,12 +103,12 @@ cat_total=$(($cis_2_1+$cis_2_2+$cis_2_3+$cis_2_4+$cis_2_5+$cis_2_6+$cis_2_7+$cis
 #echo 'section max='$max_score
 
 #generation of csv file with results
-echo '"control_name","category","subcategory","description","status","max","total"' > test.csv
-echo '"cis_2_1","cloud_trail","ensure cloudtrail enabled in all regions",'$cis_2_1','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_2","cloudtrail", "ensure cloud trail logfile validation is enabled",'$cis_2_2','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_3","s3", "ensure s3 bucket cloudtrail logs is not publicaly accesible",'$cis_2_3','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_4","cloud_trail", "Ensure CloudTrail logs are integrated with cloud watch logs",'$cis_2_4','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_5","log", "Ensure AWS Config is enabled in all regions",'$cis_2_5','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_6","s3", "Ensure s3 bucket access logging is enabled on cloudtrail s3 bucket",'$cis_2_6','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_7","encryption","Ensure Cloud trail logs are encrypted at rest using KMS CMKs",'$cis_2_7','$max_score','$cat_total'' >> test.csv
-echo '"cis_2_8","encryption","Ensure rotation for cutomer created CMKs is enabled",'$cis_2_8','$max_score','$cat_total'' >> test.csv
+echo '"control_name","category","subcategory","description","status","max","total"' > ../data/test.csv
+echo '"cis_2_1","cloud_trail","ensure cloudtrail enabled in all regions",'$cis_2_1','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_2","cloudtrail", "ensure cloud trail logfile validation is enabled",'$cis_2_2','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_3","s3", "ensure s3 bucket cloudtrail logs is not publicaly accesible",'$cis_2_3','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_4","cloud_trail", "Ensure CloudTrail logs are integrated with cloud watch logs",'$cis_2_4','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_5","log", "Ensure AWS Config is enabled in all regions",'$cis_2_5','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_6","s3", "Ensure s3 bucket access logging is enabled on cloudtrail s3 bucket",'$cis_2_6','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_7","encryption","Ensure Cloud trail logs are encrypted at rest using KMS CMKs",'$cis_2_7','$max_score','$cat_total'' >> ../data/test.csv
+echo '"cis_2_8","encryption","Ensure rotation for cutomer created CMKs is enabled",'$cis_2_8','$max_score','$cat_total'' >> ../data/test.csv
